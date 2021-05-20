1c1f5343f8d11082198841b1689f643a226592b2
Kuzin Mikhail <kuzinmv83@gmail.com>
2021-05-20 18:15:29 +0600

    вэйтер

--- docker/node/runupstream.sh
+++ docker/node/runupstream.sh
@@ -2,6 +2,15 @@
 
 npm install
 
+
+until node ./node_modules/db-migrate/bin/db-migrate up --config=config/database.json; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+
 node ./node_modules/db-migrate/bin/db-migrate up --config=config/database.json
 supervisord -c /etc/supervisor/supervisor.conf
 
